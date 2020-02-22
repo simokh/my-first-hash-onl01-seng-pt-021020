964e4c445d4b583b06578a1ab16abdc9dbab6adb
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 04:05:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  return shipping_manifest[3]
+  return shipping_manifest[2]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
